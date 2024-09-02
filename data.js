043d5afee2a54d2bd7dbe780e80661c90dae927e
radityabagass121 <radityabagass40@gmail.com>
2024-09-02 19:43:43 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const namaSaya = "raditya bagas"
+let usia = 15;
+
+function generateBiodata() {
+    if (usia > 10 && usia < 18) {
+        // KONDISI PERTAMA
+        console.log('anda remaja');
+    } else {
+        // 
+        console.log('anda anak anak')
+    }
+}
+
+console.log(namaSaya);
+console.log(usia);
+
+generateBiodata();
